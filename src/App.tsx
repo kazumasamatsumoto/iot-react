4407d867e4e3363947e77510750443ec7a22ed14
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2022-04-27 21:58:03 +0900

    動作確認OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Account, NetworkType } from 'symbol-sdk'
+
 
 function App() {
+  /* start block 01 */
+  // replace with private key
+  const privateKey =
+    '0000000000000000000000000000000000000000000000000000000000000000'
+
+  const account = Account.createFromPrivateKey(privateKey, NetworkType.TEST_NET)
+  console.log(
+    'Your account address is:',
+    account.address.pretty(),
+    'and its private key',
+    account.privateKey
+  )
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
